d7008525437073cae338dad88901fee6a406b7ca
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-10-17 16:09:52 -0400

    Formatted code in gitMain.py

--- gitMain.py
+++ gitMain.py
@@ -1,13 +1,28 @@
 from github import Github
 import git
 
-g = Github("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")
 
-repo = g.get_repo("UNF-AIRO/Experiments")
-branch = repo.get_branch("main")
-sha = branch.commit.sha
-commit = repo.get_commit(sha=sha)
-print(commit.commit.author)
+## Make a function called autoMaintain()
+## This will allow the system to maintain
+## a repository. Using the github module
+
+def autoMaintain():
+    g = Github("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")
+    
+    ## Repository Name
+    repo = g.get_repo("UNF-AIRO/Experiments")
+
+
+    ## What Branch it is on 
+    branch = repo.get_branch("main")
+
+
+    sha = branch.commit.sha
+    commit = repo.get_commit(sha=sha)
+    print(commit.commit.author)
+
+
+autoMaintain()
 
 # sha = data["pull_request"]["head"]["sha"]
 # repo.get_commit(sha=sha).create_status(
@@ -16,4 +31,3 @@ print(commit.commit.author)
 #     description="FooCI is building",
 #     context="ci/FooCI"
 # )
-
