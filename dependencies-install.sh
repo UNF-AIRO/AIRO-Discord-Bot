17ce8c95e43299e84cb187081aa0fb58ede57068
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-10-20 10:49:52 -0400

    Created the dependencies-install.sh file on AIRO-Discord-Bot

---
+++ dependencies-install.sh
@@ -0,0 +1,23 @@
+#################################################################
+#          dependencies-install.sh
+#
+#
+#
+#
+#################################################################
+
+
+
+pip3 install bs4
+
+pip3 install BeautifulSoup4
+
+pip3 install discord
+
+pip3 install python3-dotenv
+
+pip3 install xml
+
+pip3 install github 
+
+pip3 install git 
